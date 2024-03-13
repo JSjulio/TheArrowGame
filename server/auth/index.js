3125be70fd0200902e610b862df651bf99304401
Isaias Julio <isaiasjulio@Isaiass-MacBook-Air-2.local>
2024-03-13 13:54:17 -0400

    Prisma implementation pending / added ddevelopment&production environments

---
+++ server/auth/index.js
@@ -0,0 +1,99 @@
+const router = require("express").Router();
+const db = require("../db");
+const { PrismaClient } = require("@prisma/client");
+const Prisma = new PrismaClient();
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
+
+// Register a new instructor account
+router.post("/register", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const existingInstructor = await Prisma.instructor.findUnique({
+      where: {
+        username: username,
+      },
+    });
+
+    if (existingInstructor) {
+      return res.status(409).send({ error: "Username already exist" });
+    }
+    
+    const instructor = await Prisma.instructor.create({
+      data: {
+        username: username,
+        password: password,
+      },
+    });
+
+    if (instructor) {
+      const token = jwt.sign({ id: instructor.id }, JWT_SECRET);
+      res.status(200).send({ token });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Login to an existing instructor account
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username } = req.body;
+    const instructor = await Prisma.instructor.findUnique({
+      where: {
+        username: username,
+      },
+    });
+
+    if (!instructor) {
+     return res.status(401).send("Invalid credentials");
+    }
+
+    if (instructor) {
+      const token = jwt.sign({ id: instructor.id }, process.env.JWT);
+      res.status(200).send({token})
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+router.get("/me", async (req, res, next) => {
+  try {
+    const instructor = await Prisma.instructor.findUnique({
+      where: {
+        id: req.user.id
+      },
+    });
+      res.send(instructor);
+  } catch (error) {
+    console.log(error.message)
+    next(error);
+  }
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+// Get the currently logged in instructor
+// router.get("/me", async (req, res, next) => {
+//   try {
+//     const {
+//       rows: [instructor],
+//     } = await db.query("SELECT * FROM instructor WHERE id = $1", [
+//       req.user?.id,
+//     ]);
+
+//     res.send(instructor);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -1,47 +1,33 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
 
-export class Preloader extends Scene
-{
-    constructor ()
-    {
-        super('Preloader');
-    }
+export class Preloader extends Scene {
+  constructor() {
+    super("Preloader");
+  }
 
-    init ()
-    {
-        // //  This image was loaded in Boot.js 'Scene', so it can be display it here without Preload
-        // this.add.image(512, 384, 'background');
+  init() {
+    // //  This image was loaded in Boot.js 'Scene', so it can be display it here without Preload
+    // this.add.image(512, 384, 'background');
+    // //  A simple progress bar - this is the outline of the bar.
+    // this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+    // //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
+    // const bar = this.add.rectangle(512-230, 384, 4, 28, 0xffffff);
+    // //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
+    // this.load.on('progress', (progress) => {
+    //     //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
+    //     bar.width = 4 + (460 * progress);
+    // });
+  }
 
-        // //  A simple progress bar - this is the outline of the bar.
-        // this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+  preload() {
+    this.load.setPath("assets"); //when loading assests, the baseURL or initial path is given first
 
-        // //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        // const bar = this.add.rectangle(512-230, 384, 4, 28, 0xffffff);
+    this.load.image('theArrowGame', 'thearrowGame.png'); //followed by filename (logo), and the path to that file (logo.png)
+  }
 
-        // //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
-        // this.load.on('progress', (progress) => {
-
-        //     //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
-        //     bar.width = 4 + (460 * progress);
-
-        // });
-    }
-
-    preload ()
-    {
-
-        this.load.setPath('assets'); //when loading assests, the baseURL or initial path is given first 
-
-        this.load.image('theArrowGame', 'thearrowGame.png'); //followed by filename (logo), and the path to that file (logo.png)
-
-    }
-
-    create ()
-    {
-        //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
-        // this.add.image(512, 384, 'background');       
-        this.scene.start('MainMenu'); // forwards to the MainMenu scene
-
-        
-    }
+  create() {
+    //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
+    // this.add.image(512, 384, 'background');
+    this.scene.start("MainMenu"); // forwards to the MainMenu scene
+  }
 }
